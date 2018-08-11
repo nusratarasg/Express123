1fe84d17035d3c100fb6d0aa969e0e89d417815d
nusratarasg <nriaz2@my.centennialcollege.ca>
2018-08-10 21:38:53 -0400

    partials updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+// let usersRouter = require('./routes/users');
 
 let app = express();
 
@@ -15,14 +15,12 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
