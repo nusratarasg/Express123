eeae468f7af2e0d033e7901604ec8510b99bc512
nusratarasg <nriaz2@my.centennialcollege.ca>
2018-08-10 22:50:26 -0400

    Package.json updated

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'node-modules')));
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
